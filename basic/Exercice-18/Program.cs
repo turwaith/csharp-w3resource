cf60ada719b07ac324a7aef20dcdf756eba7746a
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-09-26 20:23:29 +0200

    Add basic exercice 18|19

---
+++ basic/Exercice-18/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Write a C# program to check two given integers and return true if one is negative and one is positive.
+Sample Output:
+    Input first integer:
+    -5
+    Input second integer:
+    25
+    Check if one is negative and one is positive:
+    True
+*/
+using System;
+
+namespace Exercice_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            bool isNegAndPos;
+
+            Console.Write("Input first integer: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input second integer: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+            if((firstNumber < 0 && secondNumber > 0) || (firstNumber > 0 && secondNumber < 0)) isNegAndPos = true;
+            else isNegAndPos = false;
+
+            Console.Write($"Check if one is negative and one is positive: {isNegAndPos}");
+
+        }
+    }
+}
---
+++ basic/Exercice-19/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum
+*/
+using System;
+
+namespace Exercice_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne, numberTwo, sum;
+
+            Console.Write("Enter the first number: ");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            if(numberOne == numberTwo) sum = (numberOne + numberTwo) * 3;
+            else sum = numberOne + numberTwo;
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
