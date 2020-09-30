e73c7038f8ab45c1b8061402c323d5aac4829518
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-09-30 23:18:40 +0200

    Add basic exercices 28|29

---
+++ basic/Exercice-28/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a C# program to reverse the words of a sentence. Go to the editor
+Sample Output:
+    Original String: Display the pattern like pyramid using the alphabet.
+    Reverse String: alphabet. the using pyramid like pattern the Display
+*/
+using System;
+
+namespace Exercice_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+            string[] stringWords;
+
+            Console.WriteLine("Reverse the words of a sentence.");
+            
+            Console.Write("Original String > ");
+            userInput = Console.ReadLine();
+
+            stringWords = userInput.Split(" ");
+            Array.Reverse(stringWords);
+
+            Console.WriteLine($"Reverse String  > {string.Join(" ",stringWords)}");
+        }
+    }
+}
---
+++ basic/Exercice-29/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write a C# program to find the size of a specified file in bytes.
+Sample Output:
+    Size of a file: 31
+*/
+using System;
+using System.IO;
+
+namespace Exercice_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = string.Empty;
+
+            start:
+                Console.Write("Enter the full path of the file to get its size (bytes): ");
+                inputFilePath = Console.ReadLine();
+
+                if(File.Exists(inputFilePath))
+                {
+                    FileInfo userFile = new FileInfo(inputFilePath);
+                    Console.WriteLine($"{userFile.Name} : {userFile.Length} bytes");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a correct file path");
+                    goto start;
+                }
+        }
+    }
+}
