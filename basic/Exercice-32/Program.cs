7c726f16511a943078ecdeae68e789f62e06bb23
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-03 23:41:36 +0200

    Add basic exercice 32

---
+++ basic/Exercice-32/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a C# program to create a new string of four copies, taking last four characters from a given string.
+If the length of the given string is less than 4 return the original one. 
+Sample Output:
+    Input a string : The quick brown fox jumps over the lazy dog.
+    dog.dog.dog.dog.
+*/
+using System;
+
+namespace Exercice_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+
+            Console.Write("Input a string: ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine(GetLastFourChar(userInput));            
+        }
+
+        static string GetLastFourChar(string userInput)
+        {       
+            if(userInput.Length < 4) return userInput;
+            else
+            {
+                string lastChar = userInput[^4..^0];
+                return lastChar+lastChar+lastChar+lastChar;                
+            }
+        }
+    }
+}
