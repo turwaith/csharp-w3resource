879a8fffed850b502ed5cdf9fbd5f81a2ff2df7e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-06 22:23:47 +0200

    Add basic exercice 48 code

---
+++ basic/Exercice-48/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Write a C# program to check if the first element and the last element are equal of an array of integers and the length is 1 or more.
+    Test Data:
+    Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
+    Sample Output
+    True
+*/
+using System;
+
+namespace Exercice_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
