377e31403cd8b880d24e3d6197a9e8f29c162b75
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-07 18:49:25 +0200

    Add basic exercie 48

--- basic/Exercice-48/Program.cs
+++ basic/Exercice-48/Program.cs
@@ -13,7 +13,12 @@ namespace Exercice_48
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+            
+            Console.WriteLine("Check if the first element and the last element are equal of an array of integers and the length is 1 or more.");
+            Console.WriteLine($"Array: [{string.Join(",", nums)}]");
+
+            Console.WriteLine((nums.Length >=1 && nums[0] == nums[^1]) ? true : false);
         }
     }
 }
