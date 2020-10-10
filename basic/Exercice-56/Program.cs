f10e2c49fad4ba3e7074b6b1f31b7bdd75afd7d4
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-10 23:52:52 +0200

    Add basic exercice 56

---
+++ basic/Exercice-56/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a C# program to check if a given string is a palindrome or not.
+    Sample Example:
+    For 'aaa' the output should be true
+    For 'abcd' the output should be false
+*/
+using System;
+
+namespace Exercice_56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original;
+            char[] reversed; 
+
+            Console.WriteLine("Check if a given string is a palindrome or not.");
+
+            Console.Write("Input a string: ");
+            original = string.Join("",Console.ReadLine().ToLower().Trim().Split(' '));
+
+            reversed = original.ToCharArray();            
+            Array.Reverse(reversed);
+
+            Console.WriteLine($"It's {(original.Equals(string.Join("",reversed)) ? "" : "not ")}a palindrome.");
+        }
+    }
+}
