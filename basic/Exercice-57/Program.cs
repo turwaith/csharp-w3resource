fb6c341a464c7007bd625337aec4194c01e5ddf7
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-11 20:07:35 +0200

    Add basic exercices 57|58

---
+++ basic/Exercice-57/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write a C# program to find the pair of adjacent elements that has the highest product of an given array of integers.
+*/
+using System;
+
+namespace Exercice_57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {3,9,12,3,0,3,1};
+            int max = 0,
+                maxFirstIndex = 0,
+                maxSecondIndex = 0;
+
+            Console.WriteLine("Find the pair of adjacent elements that has the highest product of an given array of integers.");
+            Console.WriteLine($"Array: [{string.Join(",", nums)}]");
+
+            for(int i = 0; i < nums.Length -1; i++)
+            {
+                if(nums[i] * nums[i+1] > max)
+                {
+                    max = nums[i] * nums[i+1];
+                    maxFirstIndex = nums[i];
+                    maxSecondIndex = nums[i+1];
+                }
+            }
+
+            Console.WriteLine($"Highest prodcut of adjacent elements: {maxFirstIndex} and {maxSecondIndex}");
+        }
+    }
+}
---
+++ basic/Exercice-58/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a C# program which will accept a list of integers and checks how many integers are needed to complete the range.
+    Sample Example [1, 3, 4, 7, 9], between 1-9 -> 2, 5, 6, 8 are not present in the list.
+    So output will be 4.
+*/
+using System;
+using System.Collections;
+
+namespace Exercice_58
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] integersList = {1, 3, 4, 7, 9};
+            int count = 0;
+
+            Console.WriteLine("Accept a list of integers and checks how many integers are needed to complete the range");
+
+            for(int i = 0; i < integersList.Length -1; i++)
+            {                
+                count += integersList[i+1] - integersList[i] - 1;
+            }
+
+            Console.WriteLine($"Integers missing: {count}");
+        }
+    }
+}
