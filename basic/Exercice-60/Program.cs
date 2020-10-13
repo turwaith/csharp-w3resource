b1e5083c9b067a8ba6947df34a108d938150c91c
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-13 20:13:20 +0200

    Add basic exercice 60 base code

---
+++ basic/Exercice-60/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a C# program to calculate the sum of all the intgers of a rectangular 
+    matrix except those integers which are located below an intger of value 0.
+    Sample Example:
+    matrix = [[0, 2, 3, 2],
+    [0, 6, 0, 1],
+    [4, 0, 3, 0]]
+    Eligible integers which will be participated to calculate the sum -
+    matrix = [[X, 2, 3, 2],
+    [X, 6, X, 1],
+    [X, X, X, X]]
+    Therefore sum will be: 2 + 3 + 2 + 6 + 1 = 14
+*/
+using System;
+
+namespace Exercice_60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array of array [,] or [][]
+            int[,] maxtrixOne = {{0,2,3,2},
+                              {0,6,0,1},
+                              {4,0,3,0}};
+
+            
+        }
+    }
+}
