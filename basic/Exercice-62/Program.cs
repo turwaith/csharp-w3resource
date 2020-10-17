0ce8b28c34bd7ab4cc6d4707fdd0081542564361
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-17 21:42:44 +0200

    Add basic exercice 62

--- basic/Exercice-62/Program.cs
+++ basic/Exercice-62/Program.cs
@@ -1,45 +1,39 @@
 ﻿/*
 Write a C# program to reverse the strings contained in each pair of matching parentheses
     in a given string and also remove the parentheses within the given string.
-    p(rq)st     =>  pqrst
-    (p(rq)st)   =>  tsrqp
+    p(rq)st         =>  pqrst
+    (p(rq)st)       =>  tsrqp
+    ab(cd(ef)gh)ij  =>  abhgefdcij
 */
 using System;
+using System.Collections;
 
 namespace Exercice_62
 {
     class Program
     {
-        static bool HasParentheses(string stringToCheck)
-        {
-            return stringToCheck.IndexOf('(') != 1 && stringToCheck.LastIndexOf(')') != -1 ? true : false;
-        }
-        static string ReversedString(string originalString)
-        {
-            //string reversedString = string.Empty;
-            while(HasParentheses(originalString))
+                static string ReversedString(string originalString)
+        {            
+            int openParentheseIndex = originalString.LastIndexOf('(');
+
+            if(openParentheseIndex == -1) return originalString;
+            else
             {
-            int firstIndex = originalString.IndexOf('('),
-                lastIndex = originalString.LastIndexOf(')');
-            
-            char[] sub = originalString.Substring(firstIndex, lastIndex).Replace("(","").Replace(")","").Trim().ToCharArray();
-            Array.Reverse(sub);
+                int closeParentheseIndex = originalString.IndexOf(')',openParentheseIndex);
+                char[] temp = originalString.Substring(openParentheseIndex+1,closeParentheseIndex - openParentheseIndex - 1).ToCharArray();
+                Array.Reverse(temp);
+                string outputString = originalString.Substring(0,openParentheseIndex) +
+                                      new string(temp) +
+                                      originalString.Substring(closeParentheseIndex+1);
+                return ReversedString(outputString);
 
-            string reversedString = string.Empty;
-            reversedString += originalString[0..firstIndex];
-            reversedString += new string(sub);
-            reversedString += originalString[(lastIndex+1)..^0];
-
-            reversedString = reversedString.Replace('(',')').Replace(')','(');
-            originalString = reversedString;
             }
-            
-            return originalString;
         }
         static void Main(string[] args)
         {
-           Console.WriteLine($"p(rq)st   => {ReversedString("p(rq)st")}");
-           Console.WriteLine($"(p(rq)st) => {ReversedString("(p(rq)st)")}");
+           Console.WriteLine($"p(rq)st        => {ReversedString("p(rq)st")}");
+           Console.WriteLine($"(p(rq)st)      => {ReversedString("(p(rq)st)")}");
+           Console.WriteLine($"ab(cd(ef)gh)ij => {ReversedString("ab(cd(ef)gh)ij")}");
         }
     }
 }
