03c18d13bcebd9455e97a36f84ccdc07fe129c25
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-25 18:26:22 +0100

    Add basic-algorithm exercice 1|2

---
+++ basic-algorithm/Exercice-01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a C# Sharp program to compute the sum of the two given integer values. 
+If the two values are the same, then return triple their sum.
+*/
+using System;
+
+namespace Exercice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInputOne = 0,
+                userInputTwo = 0;
+            
+            Console.WriteLine("Compute the sum of the two given integer values. If the two values are the same, then return triple their sum.");
+
+            Console.Write("Enter first number : ");
+            userInputOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            userInputTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Output             : {(userInputOne == userInputTwo ? 3*(userInputOne+userInputTwo) : userInputOne + userInputTwo)}");
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-02/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to get the absolute difference between n and 51.
+If n is greater than 51 return triple the absolute difference.
+*/
+using System;
+
+namespace Exercice_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0,
+                result = 0;
+
+            Console.WriteLine("Get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.");
+
+            Console.Write("Enter a number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            result = userInput > 51 ? 3*(userInput-51) : -(userInput-51);
+            
+            Console.WriteLine($"Output        : {result}");
+            
+        }
+    }
+}
