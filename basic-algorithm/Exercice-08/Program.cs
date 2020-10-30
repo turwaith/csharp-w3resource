fa5b04029f1c87559819fca97c167a41d9ed1233
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-30 22:06:15 +0100

    Add basic-algorithm exercice 8|9

---
+++ basic-algorithm/Exercice-08/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string.
+If the given string length is less than 2 return the original string
+*/
+using System;
+
+namespace Exercice_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+
+            Console.WriteLine("Create a new string which is 4 copies of the 2 front characters of a given string." +
+                             "If the given string length is less than 2 return the original string");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            if(userInput.Length < 2) Console.WriteLine(userInput);
+            else Console.WriteLine("Output           : " + userInput[0..2]+userInput[0..2]+userInput[0..2]+userInput[0..2]);
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-09/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Write a C# Sharp program to create a new string with the last char added at the front and back of a given string of length 1 or more
+*/
+using System;
+
+namespace Exercice_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+
+            Console.WriteLine("Create a new string with the last char added at the front and back of a given string of length 1 or more");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine("Output           : " + userInput[^1] + userInput + userInput[^1]);
+        }
+    }
+}
