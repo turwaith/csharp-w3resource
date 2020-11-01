5f4983290abba56435d595804149e4d66f1e774e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-01 22:24:26 +0100

    Add basic-algorithm exercice 11

--- basic-algorithm/Exercice-11/Program.cs
+++ basic-algorithm/Exercice-11/Program.cs
@@ -12,7 +12,17 @@ namespace Exercice_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput =  String.Empty;
+
+            Console.WriteLine("create a new string taking the first 3 characters of a" +
+                              "given string and return the string with the 3 characters added at both the front and back." +
+                              "If the given string length is less than 3, use whatever characters are there.");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            if(userInput.Length < 3) Console.WriteLine(userInput + userInput + userInput);
+            else Console.WriteLine(userInput[0..3] + userInput + userInput[0..3]);
         }
     }
 }
