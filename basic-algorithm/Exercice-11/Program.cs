0bf64f29caed3ca5c6691f702eb6627ee2a64a0b
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-01 21:51:47 +0100

    Add basic-algorithm exercice 11 code

---
+++ basic-algorithm/Exercice-11/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ Write a C# Sharp program to create a new string taking the first 3 characters of a
+    given string and return the string with the 3 characters added at both the front and back.
+If the given string length is less than 3, use whatever characters are there. 
+ */
+
+using System;
+
+namespace Exercice_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
