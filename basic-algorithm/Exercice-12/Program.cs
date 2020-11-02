586cb9ab90b00586844f15ac294122a5c48b9d14
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-02 19:49:02 +0100

    Add basic-algorithm exercice 12

---
+++ basic-algorithm/Exercice-12/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Write a C# Sharp program to check if a given string starts with 'C#' or not.
+*/
+using System;
+
+namespace Exercice_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+
+            Console.WriteLine("Check if a given string starts with 'C#' or not");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine(userInput[0..2] == "C#" ? "true" : "false");
+        }
+    }
+}
