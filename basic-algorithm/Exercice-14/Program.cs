7cd28fa5e34f802546e5fe2f8f9988d93d16f957
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-04 18:49:09 +0100

    Add basic-algorithm exercice 14

---
+++ basic-algorithm/Exercice-14/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a C# Sharp program to check two given integers whether either of them is in the range 100..200 inclusive
+*/
+using System;
+
+namespace Exercice_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumberOne = 0,
+                userNumberTwo = 0;
+            string userInput = String.Empty;
+
+            Console.WriteLine("Check two given integers whether either of them is in the range 100..200 inclusive");
+
+            Console.Write("Enter yours integers (coma separated): ");
+            userInput = Console.ReadLine();
+            userNumberOne = Convert.ToInt32(userInput.Split(",")[0]);
+            userNumberTwo = Convert.ToInt32(userInput.Split(",")[1]);
+
+            Console.WriteLine((userNumberOne >= 100 && userNumberOne <= 200) || (userNumberTwo >= 100 && userNumberTwo <= 200) ? "true" : "false");
+        }
+    }
+}
