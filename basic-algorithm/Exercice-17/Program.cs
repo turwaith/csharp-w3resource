770144e3057f757aea79563010349da1731d120d
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-05 18:27:42 +0100

    Add basic-algorithm exercice 17|18

---
+++ basic-algorithm/Exercice-17/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write a C# Sharp program to check if a string 'yt' appears at index 1 in a given string.
+If it appears return a string without 'yt' otherwise return the original string
+*/
+using System;
+
+namespace Exercice_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+
+            Console.WriteLine("Check if a string 'yt' appears at index 1 in a given string." +
+                              "\nIf it appears return a string without 'yt' otherwise return the original string");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+            
+            Console.WriteLine(userInput[1..3] == "yt" ? userInput[0] + userInput[2..] : userInput);
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-18/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write a C# Sharp program to check the largest number among three given integers.
+*/
+using System;
+
+namespace Exercice_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = new int[3];
+            string userInput = String.Empty;
+            int max = 0;
+
+            Console.WriteLine("Check the largest number among three given integers");
+
+            Console.Write("Enter your three integers (coma separeted): ");
+            userInput = Console.ReadLine();
+
+            userNumbers[0] = Convert.ToInt32(userInput.Split(",")[0]);
+            userNumbers[1] = Convert.ToInt32(userInput.Split(",")[1]);
+            userNumbers[2] = Convert.ToInt32(userInput.Split(",")[2]);
+
+            // whitout built in method
+            foreach(int item in userNumbers)
+                if(item > max) max = item;           
+
+            Console.WriteLine($"The largest number is                     : {max}");
+        }
+    }
+}
