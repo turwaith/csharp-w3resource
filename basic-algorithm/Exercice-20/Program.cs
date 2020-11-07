3e612eeeea78918d1e9b58764534df008bf61963
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-07 22:37:49 +0100

    Add basic-algorithm exercice 20

---
+++ basic-algorithm/Exercice-20/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to check whether two given integers are in the range 40..50 inclusive,
+    or they are both in the range 50..60 inclusive
+*/
+using System;
+
+namespace Exercice_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+            int numberOne = 0,
+                numberTwo = 0;
+
+            Console.WriteLine("Write a C# Sharp program to check whether two given integers are in the range 40..50 inclusive," +
+                              "or they are both in the range 50..60 inclusive");
+            
+            Console.Write("Enter two integers (coma separeted): ");
+            userInput = Console.ReadLine();
+            numberOne = Convert.ToInt32(userInput.Split(",")[0]);
+            numberTwo = Convert.ToInt32(userInput.Split(",")[1]);
+
+            Console.WriteLine((40 <= numberOne && numberOne <= 60) && (40 <= numberTwo && numberTwo <= 60) ? "true" : "false");
+        }
+    }
+}
