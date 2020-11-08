59802dc81d44e54e99fb970cf9d3628bd95b36c3
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-08 22:24:53 +0100

    Add basic-algorithm exercice 22

---
+++ basic-algorithm/Exercice-22/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to check if a given string contains between 2 and 4 'z' character.
+*/
+using System;
+
+namespace Exercice_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+            int count = 0;
+
+            Console.WriteLine("Check if a given string contains between 2 and 4 'z' character");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            foreach(char item in userInput)
+            {
+                if(item == 'z') count++;
+            }
+
+            Console.WriteLine(2 <= count && count <= 4 ? "true" : "false");
+        }
+    }
+}
