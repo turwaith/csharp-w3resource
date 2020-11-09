bd287cf9df3d0cc1d6f7ce3a43bfd66707a5ff8f
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-09 17:23:16 +0100

    Add basic-algorithm exercice 23

---
+++ basic-algorithm/Exercice-23/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a C# Sharp program to check if two given non-negative integers have the same last digit
+*/
+using System;
+
+namespace Exercice_23
+{
+    class Program
+    {
+        private static bool CheckLastDigit(string numberOne, string numberTwo)
+        {
+            if((Convert.ToInt32(numberOne) < 0) || (Convert.ToInt32(numberTwo) < 0)) 
+                return false;
+            else if(numberOne[^1] == numberTwo[^1])
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty,
+                   numberOne = String.Empty,
+                   numberTwo = String.Empty;
+
+            Console.WriteLine("Check if two given non-negative integers have the same last digit");
+
+            Console.Write("Enter two positive integers (coma separeted): ");
+            userInput = Console.ReadLine();
+            numberOne = userInput.Split(",")[0];
+            numberTwo = userInput.Split(",")[1];
+
+            Console.WriteLine(CheckLastDigit(numberOne, numberTwo));
+        }
+    }
+}
