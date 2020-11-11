a439277a55313d8b3fbe49361a1424eeb4230af8
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-11 18:41:00 +0100

    Add basic-algorithm exercice 27

---
+++ basic-algorithm/Exercice-27/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to count the string "aa" in a given string and assume "aaa" contains two "aa"
+*/
+using System;
+
+namespace Exercice_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+            int count = 0;
+
+            Console.WriteLine("Count the string \"aa\" in a given string and assume \"aaa\" contains two \"aa\"");
+
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            for(int i = 0; i < userInput.Length; i++)
+            {
+                if(userInput[i] == 'a' && i != (userInput.Length - 1) && userInput[i+1] == 'a') count++;
+            }
+
+            Console.WriteLine($"Total of \"aa\" in {userInput}: {count}");
+        }
+    }
+}
