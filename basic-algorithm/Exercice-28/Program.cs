cc31982ef0f208c59570d90923b7787e467a2e4a
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-12 15:45:29 +0100

    Add basic-algorithm exercice 28

---
+++ basic-algorithm/Exercice-28/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to check if the first appearance of "a" in a given string is immediately followed by another "a"
+*/
+using System;
+
+namespace Exercice_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = String.Empty;
+            int position = 0;
+
+            Console.WriteLine("Check if the first appearance of \"a\" in a given string is immediately followed by another \"a\"");
+        
+            Console.Write("Enter your string: ");
+            userInput = Console.ReadLine();
+
+            position = userInput.IndexOf("a");
+            
+            Console.WriteLine(position >= 0 && userInput[position + 1] == 'a' ? "true" : "false");
+        }
+    }
+}
