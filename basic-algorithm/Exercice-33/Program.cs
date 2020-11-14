f82892fd9031fd11ffc9f412176c30984fdde8e1
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-14 18:15:26 +0100

    Add basic-algorithm exercice 33|34

---
+++ basic-algorithm/Exercice-33/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to check if one of the first 4 elements in an array of integers is equal to a given element
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbersArray;
+            string numberToFind = String.Empty;
+
+            Console.WriteLine("Check if one of the first 4 elements in an array of integers is equal to a given element");
+
+            Console.Write("Enter your array (coma separeted): ");
+            numbersArray = Console.ReadLine().Split(",");
+
+            Console.Write("Enter the number to find: ");
+            numberToFind = Console.ReadLine();
+
+            Console.WriteLine(numbersArray[0..4].Contains(numberToFind));
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-34/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a C# Sharp program to check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_34
+{
+    class Program
+    {
+        private static bool SerieIsInside(string[] numbersArray)
+        {
+            for(int i = 0; i < numbersArray.Length; i++)            
+                if(i <= (numbersArray.Length - 3) && numbersArray[i] == "1" && numbersArray[i+1] =="2" && numbersArray[i+2] == "3")
+                    return true;
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            string[] numbersArray;
+
+            Console.WriteLine("Check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere");
+
+            Console.Write("Enter your array (coma separeted): ");
+            numbersArray = Console.ReadLine().Split(",");
+
+            Console.WriteLine(SerieIsInside(numbersArray));
+        }
+    }
+}
