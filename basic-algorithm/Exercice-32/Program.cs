12d7e3e35e3458b91ae56ed1d500aae89e695c96
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-14 17:32:10 +0100

    Add basic-algorithm exercice 32

---
+++ basic-algorithm/Exercice-32/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to check a specified number is present in a given array of integers
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string numberToFind = String.Empty;
+            string[] numbersArray;
+
+            Console.WriteLine("Check a specified number is present in a given array of integers");
+        
+            Console.Write("Enter your array (coma separeted): ");
+            numbersArray = Console.ReadLine().Split(",");            
+
+            Console.Write("Enter the number to find: ");
+            numberToFind =  Console.ReadLine();
+
+            Console.WriteLine(numbersArray.Contains(numberToFind));
+        }
+    }
+}
