5fc20c3ceac2686cd66728322061cd454b66210c
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-15 17:46:17 +0100

    Add basic-algorithm exercice 35 correction + optimization

--- basic-algorithm/Exercice-35/Program.cs
+++ basic-algorithm/Exercice-35/Program.cs
@@ -11,41 +11,25 @@ namespace Exercice_35
     {
         static void Main(string[] args)
         {
-            string userInput = String.Empty,
-                   userLargerString = String.Empty,
-                   userSmallerString = String.Empty;
+            string userFirstInput = String.Empty,
+                   userSecondInput = String.Empty;
             int count = 0;
 
             Console.WriteLine("Compare two given strings and return the number of the positions\n "+
                               "where they contain the same length 2 substring");
 
             Console.Write("Enter the first string: ");
-            userInput = Console.ReadLine();
-            userSmallerString = userInput;
+            userFirstInput = Console.ReadLine();            
 
             Console.Write("Enter the second string: ");
-            userInput = Console.ReadLine();
-            if(userInput.Length < userSmallerString.Length)
-            {
-                userLargerString = userSmallerString;
-                userSmallerString = userInput;
-            }
-            else userLargerString = userInput;
+            userSecondInput = Console.ReadLine();
             
-            //Console.WriteLine($"Smaller {userSmallerString} \nLarger {userLargerString}");
-
-            for(int i = 0; i < userSmallerString.Length-1; i+=2)
+            for(int i = 0; i < userFirstInput.Length-1; i++)
             {                
-                if(userLargerString.Contains(userSmallerString[i..(i+2)])) count++;
+                if(userSecondInput.Contains(userFirstInput[i..(i+2)])) count++;
             }            
 
             Console.WriteLine($"Output: {count}");
         }
     }
 }
-/*
-for(int j = 0; j <   userLargerString.Length; j+=2)
-                {
-                    if(userSmallerString[i..(i+2)] == userLargerString[i..(j+2)]) count++;
-                }
-                */
