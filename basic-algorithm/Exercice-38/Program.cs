be3193cb0a8e8c879e9afe6655e13b252ea2b3ac
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-17 16:46:17 +0100

    Add basic-algorithm exercice 38

---
+++ basic-algorithm/Exercice-38/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to count the number of two 5's are next to each other in an array of integers.
+Also count the situation where the second 5 is actually a 6
+*/
+using System;
+
+namespace Exercice_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNumbers;
+            int count = 0;
+
+            Console.WriteLine("Count the number of two 5's are next to each other in an array of integers." +
+                              "\nAlso count the situation where the second 5 is actually a 6");
+        
+            Console.Write("Enter your numbers (coma separeted): ");
+            userNumbers = Console.ReadLine().Split(",");
+
+            for(int i = 0; i <= (userNumbers.Length - 2); i++)            
+                if(userNumbers[i] == "5" && (userNumbers[i+1] == "5" || userNumbers[i+1] == "6")) count++;  
+
+            Console.WriteLine($"Output: {count}");
+        }
+    }
+}
