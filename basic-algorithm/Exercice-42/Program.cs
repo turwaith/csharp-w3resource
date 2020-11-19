2546d98a6c352f81eb6f0d29d2e4a7f2aecca035
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-19 18:09:07 +0100

    Add basic-algorithm exercice 42

---
+++ basic-algorithm/Exercice-42/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to test if a given non-negative number is a multiple of 13 or it is one more than a multiple of 13
+*/
+using System;
+
+namespace Exercice_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            bool isMultiple = false;
+
+            Console.WriteLine("Test if a given non-negative number is a multiple of 13 or it is one more than a multiple of 13");
+        
+            Console.Write("Enter an integer: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            isMultiple = (Math.Abs(userInput) % 13 == 0) || (Math.Abs(userInput) % 13 == 1) ? true : false;
+
+            Console.WriteLine($"Output: {isMultiple}");
+        }
+    }
+}
