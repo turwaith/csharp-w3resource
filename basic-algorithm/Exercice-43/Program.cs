f21a70d07073b8dba00fe0ed5931b4d748f8f935
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-20 18:11:18 +0100

    Add basic-algorithm exercice 43

---
+++ basic-algorithm/Exercice-43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a C# Sharp program to check if a given non-negative given number is a multiple of 3 or 7, but not both
+*/
+using System;
+
+namespace Exercice_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            bool isMultiple =  false;
+
+            Console.WriteLine("Check if a given non-negative given number is a multiple of 3 or 7, but not both");
+        
+            Console.Write("Enter your number (non-negative): ");
+            userInput = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            if((userInput % 7 == 0) && (userInput % 3 == 0)) isMultiple = false;
+            else if((userInput % 7 == 0) || (userInput % 3 == 0)) isMultiple = true;
+
+            Console.WriteLine($"Output {isMultiple}");
+        }
+    }
+}
