b0b1164754e8b2cb361ce6888c21e57ad3c1de55
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-24 19:12:45 +0100

    Add basic-algorithm exercice 47

---
+++ basic-algorithm/Exercice-47/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write a C# Sharp program to check if it is possible to add two integers to get the third integer from three given integers
+*/
+using System;
+
+namespace Exercice_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userInput;
+            bool output = false;
+
+            Console.WriteLine("Check if it is possible to add two integers to get the third integer from three given integers");
+        
+            Console.Write("Enter 3 integers (coma separeted): ");
+            userInput = Console.ReadLine().Split(",");
+            output = Convert.ToInt32(userInput[0]) + Convert.ToInt32(userInput[1]) == Convert.ToInt32(userInput[2]);
+
+            Console.WriteLine($"Output: {output}");
+        }
+    }
+}
