17fd29e1b7a9e1bc284f3dad8d479655d15fca15
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-27 18:10:50 +0100

    Add basic-algorithm exercice 50

---
+++ basic-algorithm/Exercice-50/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Write a C# Sharp program to check if two or more non-negative given integers have the same rightmost digit
+*/
+using System;
+
+namespace Exercice_50
+{
+    class Program
+    {
+        public static bool RightMost(int a, int b, int c)
+        {
+            return a%10 == b%10 || a%10 == c%10 || b%10 == c%10;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if two or more non-negative given integers have the same rightmost digit");
+
+            Console.WriteLine(RightMost(11,21,31));
+            Console.WriteLine(RightMost(11,22,31));
+            Console.WriteLine(RightMost(11,22,33));
+        }
+    }
+}
