c9dd06a7ea94de70a27c4fb22d9275f9d337bad3
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-11-28 21:55:02 +0100

    Add basic-algorithm exercice 51

---
+++ basic-algorithm/Exercice-51/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a C# Sharp program to check three given integers and return true if one of them is 20 or more less than one of the others
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_51
+{
+    class Program
+    {
+        public static bool IsLess(int a, int b, int c, int d = 20 )
+        {
+            int[] numbers = {a,b,c};
+            return numbers.Min() + d <= numbers.Max();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check three given integers and return true if one of them is 20 or more less than one of the others");
+
+            Console.WriteLine(IsLess(11,21,31));
+            Console.WriteLine(IsLess(11,22,31));
+            Console.WriteLine(IsLess(10,20,15));
+            Console.WriteLine(IsLess(20,25,0));
+        }
+    }
+}
