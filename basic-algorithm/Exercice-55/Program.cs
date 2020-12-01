e6f627e1c4b56998a554de7551af80e20f7a9d7e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-01 18:58:12 +0100

    Add basic-algorihtm exercice 55|56

---
+++ basic-algorithm/Exercice-55/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Write a C# Sharp program to compute the sum of three given integers.
+If the two values are same return the third value
+*/
+using System;
+
+namespace Exercice_55
+{
+    class Program
+    {
+        public static int GetSum(int a, int b, int c)
+        {
+            if(a == b) return c;
+            if(b == c) return a;
+            if(a == c) return b;
+            return a == b ? c : a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Compute the sum of three given integers.
+If the two values are same return the third value");
+        
+            Console.WriteLine(GetSum(4,5,7));
+            Console.WriteLine(GetSum(7,4,12));
+            Console.WriteLine(GetSum(10,10,12));
+            Console.WriteLine(GetSum(12,12,18));
+            Console.WriteLine(GetSum(12,10,12));
+            Console.WriteLine(GetSum(1,12,12));
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-56/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Write a C# Sharp program to compute the sum of the three integers. 
+If one of the values is 13 then do not count it and its right towards the sum
+*/
+using System;
+
+namespace Exercice_56
+{
+    class Program
+    {
+        static public int GetSum(int a, int b, int c)
+        {
+            if(a == 13) return 0;
+            if(b == 13) return a;
+            if(c == 13) return a + b;
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Compute the sum of the three integers. 
+If one of the values is 13 then do not count it and its right towards the sum");
+
+            Console.WriteLine(GetSum(4,5,7));
+            Console.WriteLine(GetSum(7,4,12));
+            Console.WriteLine(GetSum(10,13,12));
+            Console.WriteLine(GetSum(13,12,18));
+            Console.WriteLine(GetSum(4,5,13));
+
+        }
+    }
+}
