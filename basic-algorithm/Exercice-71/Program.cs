c5bce885cfdb8889d627aff07a918563297f6fe6
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-07 17:54:29 +0100

    Add basic-algorithm exercice 71

---
+++ basic-algorithm/Exercice-71/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new string using the two middle
+    characters of a given string of even length (at least 2)
+*/
+using System;
+
+namespace Exercice_71
+{
+    class Program
+    {
+        public static string NewString(string original)
+        {
+            return original.Length >= 2 ?
+                original.Substring((original.Length - 1) / 2, 2) :
+                original;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new string using the two middle\n" +
+                              "characters of a given string of even length (at least 2)");
+        
+            Console.WriteLine(NewString("Hell"));
+            Console.WriteLine(NewString("JS"));
+            Console.WriteLine(NewString("String"));
+        }
+    }
+}
