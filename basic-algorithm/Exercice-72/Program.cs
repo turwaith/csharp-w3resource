8dbc3ec78be748b1807ec7c747e5b9953b867854
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-08 18:23:52 +0100

    Add basic-algorithm exercice 72|73

---
+++ basic-algorithm/Exercice-72/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write a C# Sharp program to check if a given string ends with "on"
+*/
+using System;
+
+namespace Exercice_72
+{
+    class Program
+    {
+        public static bool EndsWithOn(string stringToVerify)
+        {
+            return stringToVerify.EndsWith("on");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if a given string ends with \"on\"");
+
+            Console.WriteLine(EndsWithOn("Hello"));
+            Console.WriteLine(EndsWithOn("Python"));
+            Console.WriteLine(EndsWithOn("on"));
+            Console.WriteLine(EndsWithOn("o"));
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-73/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new string using the first
+    and last n characters from a given string of length at least n
+*/
+using System;
+
+namespace Exercice_73
+{
+    class Program
+    {
+        public static string NewString(string original, int number)
+        {
+            if(original.Length == 1) return original + original;
+            return original[0..(number)] + original[^number..];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new string using the first and last n\n" +
+                              "characters from a given string of length at least n");
+
+            Console.WriteLine(NewString("Hello", 1));
+            Console.WriteLine(NewString("Python", 2));
+            Console.WriteLine(NewString("on", 1));
+            Console.WriteLine(NewString("o", 1));
+        }
+    }
+}
