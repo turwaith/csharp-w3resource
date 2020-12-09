cb33f994af536eae21319eb2778b7d8de2b5fe48
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-09 17:56:20 +0100

    Add basic-algorithm exercice 74|75

---
+++ basic-algorithm/Exercice-74/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Write a C# Sharp program to create a new string of length 2
+    starting at the given index of a given string
+*/
+using System;
+
+namespace Exercice_74
+{
+    class Program
+    {
+        public static string NewString(string original, int index)
+        {
+            return original[index..(index + 2)];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new string of length 2 starting at the given index of a given string");
+
+            Console.WriteLine(NewString("Hello", 1));
+            Console.WriteLine(NewString("Python", 2));
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-75/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write a C# Sharp program to create a new string taking 3 characters
+    from the middle of a given string at least 3
+ */
+using System;
+
+namespace Exercice_75
+{
+    class Program
+    {
+        public static string NewString(string original)
+        {
+            if(original.Length <= 3) return original;
+            else
+            {
+                int middle = original.Length % 2 == 0 ?
+                    (original.Length /2) - 2 :
+                    (original.Length /2) - 1;
+                return original[middle..(middle +3)];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new string taking 3 characters\n" +
+                              "from the middle of a given string at least 3");
+
+            Console.WriteLine(NewString("Hello"));
+            Console.WriteLine(NewString("Python"));
+            Console.WriteLine(NewString("abc"));
+        }
+    }
+}
