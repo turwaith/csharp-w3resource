3d462130f6a0b754f6db8ca3357712685d215e9a
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-10 19:27:42 +0100

    Add basic-algorithm exercice 76

---
+++ basic-algorithm/Exercice-76/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Write a C# Sharp program to create a new string of length 2,
+    using first two characters of a given string.
+If the given string length is less than 2 use '#'
+    as missing characters
+*/
+using System;
+
+namespace Exercice_76
+{
+    class Program
+    {
+        public static string NewString(string original)
+        {
+            if(original.Length <= 2) return original + new string('#',(2 - original.Length));
+            return original[0..2];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new string of length 2,
+    using first two characters of a given string.
+If the given string length is less than 2 use '#'
+    as missing characters");
+
+            Console.WriteLine(NewString("Hello"));
+            Console.WriteLine(NewString("Python"));
+            Console.WriteLine(NewString("a"));
+            Console.WriteLine(NewString(""));
+        }
+    }
+}
