22f9bebf6ee0e662c6d1e864cfeffc132eb7203c
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-12 12:53:21 +0100

    Add basic-algorithm exercice 78

---
+++ basic-algorithm/Exercice-78/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to concat two given strings (lowercase).
+If there are any double character in new string then omit one character
+*/
+using System;
+
+namespace Exercice_78
+{
+    class Program
+    {
+        public static string NewString(string originalOne, string originalTwo)
+        {
+            return originalOne[^1] == originalTwo[0] ?
+                originalOne + originalTwo[1..] :
+                originalOne + originalTwo;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Concat two given strings (lowercase).
+If there are any double character in new string then omit one character");
+
+            Console.WriteLine(NewString("abc","cat"));
+            Console.WriteLine(NewString("python", "php"));
+            Console.WriteLine(NewString("php", "php"));            
+        }
+    }
+}
