e899a76a2ad0198abf764b884ce6db20d1044f47
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-15 18:08:37 +0100

    Add basic-algorithm exercice 83

---
+++ basic-algorithm/Exercice-83/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a C# Sharp program to create a new string using 3 copies of the first 2
+    characters of a given string.
+If the length of the given string is less than 2 use the whole string
+ */
+using System;
+
+namespace Exercice_83
+{
+    class Program
+    {
+        public static string NewString(string original)
+        {
+            return original.Length <= 2 ?
+                original + original + original :
+                original[0..2] + original[0..2] + original[0..2];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new string using 3 copies of the first 2
+    characters of a given string.
+If the length of the given string is less than 2 use the whole string");
+
+            Console.WriteLine(NewString("abc"));
+            Console.WriteLine(NewString("Python"));
+            Console.WriteLine(NewString("J"));
+        }
+    }
+}
