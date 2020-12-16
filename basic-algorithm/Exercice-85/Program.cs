3c3b188687bb6df306ce4e7789439c114e43e95e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-16 19:13:43 +0100

    Add basic-algorithm exercice 85

---
+++ basic-algorithm/Exercice-85/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write a C# Sharp program to create a new string from a given
+    string without the first two characters.
+Keep the first character if it is "p" and keep the
+    second character if it is "y"
+*/
+using System;
+
+namespace Exercice_85
+{
+    class Program
+    {
+        public static string NewString(string original)
+        {
+            if(original[0] == 'p' && original[1] == 'y') return original;
+            else if(original[0] == 'p') return original[0] + original[2..];
+            else if(original[1] == 'y') return original[1..];
+            else return original[2..];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new string from a given
+    string without the first two characters.
+Keep the first character if it is ""p"" and keep the
+    second character if it is ""y""");
+        
+            Console.WriteLine(NewString("abcab"));
+            Console.WriteLine(NewString("python"));
+            Console.WriteLine(NewString("press"));
+            Console.WriteLine(NewString("jython"));
+        }
+    }
+}
