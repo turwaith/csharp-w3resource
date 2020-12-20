d74eb5e1d17efe78d3ff2865ae31e726c431d5f1
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-20 17:29:35 +0100

    Add basic-algorithm exerice 90

---
+++ basic-algorithm/Exercice-90/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to check two given arrays of integers of
+    length 1 or more and return true if they have the same first
+    element or they have the same last element
+*/
+using System;
+
+namespace Exercice_90
+{
+    class Program
+    {
+        public static bool IsStartEndEqual(int[] numbersOne, int[] numbersTwo)
+        {
+            return numbersOne[0] == numbersTwo[0] ||
+                   numbersOne[^1] == numbersTwo[^1];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Check two given arrays of integers of
+    length 1 or more and return true if they have the same first
+    element or they have the same last element");
+
+            Console.WriteLine(IsStartEndEqual(new int[]{10,20,40,50 },new int[]{10,20,40,50}));
+            Console.WriteLine(IsStartEndEqual(new int[]{10,20,40,50 },new int[]{10,20,40,5}));
+            Console.WriteLine(IsStartEndEqual(new int[]{10,20,40,50 },new int[]{1,20,40,5}));
+        }
+    }
+}
