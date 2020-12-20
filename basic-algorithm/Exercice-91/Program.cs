41de236921d86c91ec43fda890c9a5b2e2fb2800
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-20 17:33:51 +0100

    Add basic-algorithm exercice 91

---
+++ basic-algorithm/Exercice-91/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Write a C# Sharp program to compute the sum of the elements
+    of an given array of integers
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Compute the sum of the elements
+    of an given array of integers");
+            
+            Console.WriteLine(new int[]{10,20,30,40,50}.Sum());
+            Console.WriteLine(new int[]{10,20,-30,-40,50}.Sum());
+        }
+    }
+}
