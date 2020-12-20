7fc7e771c222203d1aaf1a883d9559a44ceb36dc
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-20 17:28:39 +0100

    Add basic-algorithm exercice 89

---
+++ basic-algorithm/Exercice-89/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to check a given array of integers of length
+    1 or more and return true if the first element and the
+    last element are equal in the given array
+*/
+using System;
+
+namespace Exercice_89
+{
+    class Program
+    {
+        public static bool IsStartEndEqual(int[] numbers)
+        {
+            return numbers[0] == numbers[^1];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Check a given array of integers of length
+    1 or more and return true if the first element and the
+    last element are equal in the given array");
+        
+        Console.WriteLine(IsStartEndEqual(new int[]{10,20,40,50}));
+        Console.WriteLine(IsStartEndEqual(new int[]{10,20,40,10}));
+        Console.WriteLine(IsStartEndEqual(new int[]{12,24,35,55}));
+        }
+    }
+}
