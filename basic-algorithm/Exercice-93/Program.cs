71aca5345f6e7fd38dd52fbc4bf376019d4bd0c0
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-21 18:53:39 +0100

    Add basic-algorithm exercice 93

---
+++ basic-algorithm/Exercice-93/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Write a C# Sharp program to reverse a given array of integers and length 5
+*/
+using System;
+
+namespace Exercice_93
+{
+    class Program
+    {
+        public static int[] GetReversedArray(int[] numbers)
+        {
+            Array.Reverse(numbers);
+            return numbers;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse a given array of integers and length 5");
+
+            Console.WriteLine($"{{{String.Join(",",GetReversedArray(new int[]{ 10, 20, -30, -40, 50 }))}}}");
+        }
+    }
+}
