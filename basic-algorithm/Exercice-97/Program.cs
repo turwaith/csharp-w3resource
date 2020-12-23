5eb4973beef8af4e750d914c9b60857ecf08a86e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-23 15:52:08 +0100

    Add basic-algorithm exercice 97

---
+++ basic-algorithm/Exercice-97/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write a C# Sharp program to check if a given array of integers and length 2, contains 15 or 20
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_97
+{
+    class Program
+    {
+        public static bool IsNumberInside(int[] numbers, int a = 15, int b = 20)
+        {
+            return numbers.Contains(a) || numbers.Contains(b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if a given array of integers and length 2, contains 15 or 20");
+
+            Console.WriteLine(IsNumberInside(new int[] {12,20}));
+            Console.WriteLine(IsNumberInside(new int[] {14,15}));
+            Console.WriteLine(IsNumberInside(new int[] {11,21}));
+        }
+    }
+}
