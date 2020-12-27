be546d360d0f40a56d51ccab87f7568fdf2058f9
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-27 15:44:30 +0100

    Add basic-algorithm exercice 105

---
+++ basic-algorithm/Exercice-105/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write a C# Sharp program to create a new array swapping the first
+    and last elements of a given array of integers and length will be least 1
+*/
+using System;
+
+namespace Exercice_105
+{
+    class Program
+    {
+        public static void GetNewArray(int[] original)
+        {
+            int[] output = new int[original.Length];
+            
+            original.CopyTo(output,0);
+            output[0] = original[^1];
+            output[^1] = original[0];
+            
+            Console.WriteLine($"Original  array : {{{String.Join(",",original)}}}");
+            Console.WriteLine($"Swapped  array  : {{{String.Join(",",output)}}}");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new array swapping the first
+    and last elements of a given array of integers and length will be least 1");
+
+            GetNewArray(new int[]{1,5,7,9,11,13});
+            GetNewArray(new int[]{19,33,0,5,69,88});
+        }
+    }
+}
