42e63ba6e625320b4f4e665fd0322886a5e07e41
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-12-31 14:06:27 +0100

    Add basic-algorithm exercice 110

---
+++ basic-algorithm/Exercice-110/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to compute the difference between the largest and
+    smallest values in a given array of integers and length one or more
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_110
+{
+    class Program
+    {
+        public static int GetDifference(int[] numbers)
+        {
+            Console.WriteLine($"Original array: {{{String.Join(",", numbers)}}}");
+            return numbers.Max() - numbers.Min();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Compute the difference between the largest and
+    smallest values in a given array of integers and length one or more");
+
+            Console.WriteLine($"Difference between the largest and smallest values: {GetDifference(new int[]{1, 5, 7, 9, 10, 12})}");
+        }
+    }
+}
