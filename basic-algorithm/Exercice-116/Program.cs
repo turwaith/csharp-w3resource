8a1b8cc9a29a1917c6bfa44b3412b02a27c2dc30
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-02 17:20:56 +0100

    Add basic-algorithm exercice 116

---
+++ basic-algorithm/Exercice-116/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Write a C# Sharp program to check if the number of 3's is greater than the number of 5's
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_116
+{
+    class Program
+    {
+        public static bool IsGreater(int[] numbers)
+        {
+            return numbers.Count(item => item == 3) > numbers.Count(item => item == 5);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if the number of 3's is greater than the number of 5's");
+
+            Console.WriteLine(IsGreater(new int[] {1,5,6,9,3,3}));
+            Console.WriteLine(IsGreater(new int[] {1,5,5,5,10,17}));
+            Console.WriteLine(IsGreater(new int[] {1,3,3,5,5,5}));
+        }
+    }
+}
