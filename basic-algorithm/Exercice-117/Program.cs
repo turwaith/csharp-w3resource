d279c81d327cbf7321d0055b9c1e3345fa31ca8b
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-03 12:40:27 +0100

    Add basic-algorithm exercice 117

---
+++ basic-algorithm/Exercice-117/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to check if a given array of integers contains a 3 or a 5
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_117
+{
+    class Program
+    {
+        public static bool IsInside(int[] numbers)
+        {
+            return numbers.Contains(3) || numbers.Contains(5);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if a given array of integers contains a 3 or a 5");
+        
+            Console.WriteLine(IsInside(new int[]{5,5,5,5,5}));
+            Console.WriteLine(IsInside(new int[]{3,3,3,3}));
+            Console.WriteLine(IsInside(new int[]{3,3,3,5,5,5}));
+            Console.WriteLine(IsInside(new int[]{1,6,8,10}));
+        }
+    }
+}
