ae5fa02706517f1eb23c7ae8da57d49cf1b70c47
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-07 19:24:26 +0100

    Add basic-algorithm exercice 124 base code

---
+++ basic-algorithm/Exercice-124/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write a C# Sharp program to check a given array of integers and return
+    true if every 5 that appears in the given array is next to another 5
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_124
+{
+    class Program
+    {
+        public static bool IsNext(int[] numbers, int nb = 5)
+        {
+            bool isNbNextNb = false;
+
+            for(int i = Array.IndexOf(numbers, nb); numbers.Length-1; i++)
+            {
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Check a given array of integers and return
+    true if every 5 that appears in the given array is next to another 5");
+
+            Console.WriteLine(IsNext(new int[]{3,5,5,3,7}));
+            Console.WriteLine(IsNext(new int[]{3,5,5,4,1,5,7}));
+            Console.WriteLine(IsNext(new int[]{3,5,5,5,5,5}));
+            Console.WriteLine(IsNext(new int[]{2,4,5,5,6,7,5}));
+        }
+    }
+}
