59d348366b01e0c528407f971b7ab05c0b47ae74
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-08 21:57:31 +0100

    Add basic-algorithm exercice 125 base code

---
+++ basic-algorithm/Exercice-125/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Write a C# Sharp program to check a given array of integers and return
+    true if the specified number of same elements appears at the start
+    and end of the given array
+*/
+using System;
+
+namespace Exercice_125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Check a given array of integers and return
+    true if the specified number of same elements appears at the start
+    and end of the given array");
+        }
+    }
+}
