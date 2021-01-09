03e562500b6ea68a20eb134261968751eeb1c3d7
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-09 17:30:33 +0100

    Add basic-algorithm exercice 125

--- basic-algorithm/Exercice-125/Program.cs
+++ basic-algorithm/Exercice-125/Program.cs
@@ -9,11 +9,25 @@ namespace Exercice_125
 {
     class Program
     {
+        public static bool IsSame(int[] numbers, int nElement)
+        {
+            int[] a = numbers[0..(nElement)],
+                  b = numbers[^(nElement)..];
+            
+            for(int i = 0; i < a.Length; i++)
+                if(a[i] != b[i]) return false;
+            
+            return true;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine(@"Check a given array of integers and return
     true if the specified number of same elements appears at the start
     and end of the given array");
+
+            Console.WriteLine(IsSame(new int[]{3,7,5,5,3,7},2));
+            Console.WriteLine(IsSame(new int[]{3,7,5,5,3,7},3));
+            Console.WriteLine(IsSame(new int[]{3,7,5,5,3,7,5},3));              
         }
     }
 }
