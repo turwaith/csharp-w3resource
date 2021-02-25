3b5410c668cd0bdd06ff044c30ce7a55e9c51f12
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-02-25 23:06:56 +0100

    Add basic-algorithm exercice 129

---
+++ basic-algorithm/Exercice-129/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Write a C# Sharp program to create a new array taking the elements
+    after the element value 5 from a given array of integers
+*/
+using System;
+
+namespace Exercice_129
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new array taking the elements
+    after the element value 5 from a given array of integers");
+
+            GetNextFive one = new GetNextFive(new int[]{1,2,3,5,7,9,11});
+
+            Console.WriteLine($"New array: {one}");
+
+        }
+    }
+
+    class GetNextFive
+    {
+        private int[] Old;
+
+        public GetNextFive(int[] numbers)
+        {
+            Old = numbers;
+        }
+
+        public override string ToString()
+        {
+            return $"[{String.Join(",",Old[(Array.IndexOf(Old, 5)+1)..])}]";
+        }
+    }
+}
