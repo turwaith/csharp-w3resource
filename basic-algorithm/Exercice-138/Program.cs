80b7090a49e5e2767215df0b88fe7a8dc4029661
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-06 22:05:53 +0100

    Add basic-algorithm exercice-138

---
+++ basic-algorithm/Exercice-138/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a C# Sharp program to create a new array from a given array of strings
+    using all the strings whose length are matched with given string length
+*/
+using System;
+using System.Linq;
+
+namespace Exercice_138
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new array from a given array of strings
+    using all the strings whose length are matched with given string length");
+
+            Console.WriteLine(String.Join(",",GetNewArray(new string[]{"a", "aaa", "b", "bbb", "c", "ccc" }, 3)));
+        }
+
+        static string GetNewArray(string[] sentences, int size)
+        {
+            string[] output = sentences.Where(item => item.Length == size).ToArray();
+
+            Console.Write($"[{String.Join(",",sentences)}], ({size}) -> ");
+
+            return $"[{String.Join(",",output)}]";
+        }
+    }
+}
