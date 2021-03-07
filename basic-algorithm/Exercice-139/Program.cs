893bbb49c7ff8f057990d5a2ff970d02afc8db5e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-07 15:21:25 +0100

    Add basic-algorithm exercice 139

---
+++ basic-algorithm/Exercice-139/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Write a C# Sharp program to check a positive integer and return true if it contains a number 2
+*/
+using System;
+
+namespace Exercice_139
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Check a positive integer and return true if it contains a number 2");
+
+            Console.WriteLine(123.ContainsNumber(2));
+            Console.WriteLine(13.ContainsNumber(2));
+            Console.WriteLine(222.ContainsNumber(2));
+        }
+
+    }
+    public static class Extension
+    {
+        public static bool ContainsNumber(this int numbers, int number)
+        {
+            string nb = numbers.ToString();
+
+            Console.Write($"{nb} -> ");
+            
+            return nb.Contains(number.ToString());
+        }
+    }
+}
