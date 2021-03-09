734caa3283b6cf7b873715a05f08e576b1117f71
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-09 14:16:14 +0100

    Add basic-algorithm exercice 141|142

---
+++ basic-algorithm/Exercice-141/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of integers where each element is multiplied by 3
+*/
+using System;
+
+namespace Exercice_141
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of 
+integers where each element is multiplied by 3");
+
+            Console.WriteLine($"[{String.Join(",",Multiply(new int[]{ 1, 2, 3 , 4 },3))}]");
+        }
+
+        static int[] Multiply(int[] numbers, int multiplier)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+
+            for(int i = 0, max = numbers.Length; i < max; i++)
+                numbers[i] *= multiplier;
+            
+            return numbers;
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-142/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given
+    list of integers where each integer multiplied by itself three times
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of integers
+    where each integer multiplied by itself three times");
+
+            Console.WriteLine($"[{String.Join(",",GetNewList(new List<int>(){ 1, 2, 3 , 4 }))}]");
+        }
+
+        static List<int> GetNewList(List<int> numbers)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+
+            return numbers.Select(item => (int)Math.Pow(item,3)).ToList();
+        }
+    }
+}
