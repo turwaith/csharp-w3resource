d51d646f6ae31250c97653267ac82e39aa7426d4
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-10 15:17:44 +0100

    Add basic-algorithm exercice 143|144

---
+++ basic-algorithm/Exercice-143/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of
+    strings where each element has "#" added at the beginning and end position
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of strings where each
+    element has ""#"" added at the beginning and end position");
+
+            Console.WriteLine($"[{String.Join(",",ChangeItem(new List<string>(){ "1", "2", "3" , "4" }))}]");
+        }
+
+        static List<string> ChangeItem(List<string> sentences, char sep = '#')
+        {
+            Console.Write($"[{String.Join(",",sentences)}] -> ");
+            return sentences.Select(item => sep+item+sep).ToList();
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-144/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of strings where each
+element is replaced by 4 copies of the string concatenated together
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of strings where each
+element is replaced by 4 copies of the string concatenated together");
+
+            Console.WriteLine($"[{String.Join(",",RepeatItem(new List<string>(){"1","2","3","4"}))}]");
+        }
+
+        static List<string> RepeatItem(List<string> sentences, int times = 4)
+        {
+            Console.Write($"[{String.Join(",",sentences)}] -> ");
+            return sentences.Select(item => new string(item[0],times)).ToList();
+        }
+    }
+}
