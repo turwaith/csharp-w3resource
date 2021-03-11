c0aba757425cd1bfd870b8252cefc401cdc2988a
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-11 12:19:20 +0100

    Add basic-algorithm exercice 145|146

---
+++ basic-algorithm/Exercice-145/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of integers where each
+    integer value is added to 2 and the result value is multiplied by 5
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of integers where each
+integer value is added to 2 and the result value is multiplied by 5");
+
+            Console.WriteLine($"[{String.Join(",",Multiply(new List<int>(){1,2,3,4}))}]");
+        }
+
+        static List<int> Multiply(List<int> numbers, int multiplier = 5)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+            return numbers.Select(item => (item + 2) * multiplier).ToList();
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-146/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a C# Sharp program to create a new list of the rightmost digits from a given list of positive integers
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new list of the rightmost digits from a given list of positive integers");
+
+            Console.WriteLine($"[{String.Join(",",GetRightDigit(new List<int>(){ 10, 22, 35 , 41 }))}]");
+            Console.WriteLine($"[{String.Join(",",GetRightDigit(new List<int>(){ 21, 99, 165 , 0 }))}]");
+        }
+
+        static List<int> GetRightDigit(List<int> numbers)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+            // cast to String, then get last char and return its numeric value
+            // return numbers.Select(item => (int)Char.GetNumericValue(item.ToString()[^1])).ToList();
+            return numbers.Select(item => item % 10).ToList();
+        }
+    }
+}
