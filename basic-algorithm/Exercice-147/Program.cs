35bafe48357efc0927a12f7c56a93053dc8a7391
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-12 12:45:45 +0100

    Add basic-algorithm exercice 147|148

---
+++ basic-algorithm/Exercice-147/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of
+    strings where strings will be in upper case in new string
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_147
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of
+strings where strings will be in upper case in new string");
+
+            Console.WriteLine($"[{String.Join(",",UpperCase(new List<string>(){"Abc","cdef","js","php"}))}]");
+        }
+
+        static List<string> UpperCase(List<string> sentences)
+        {
+            Console.Write($"[{String.Join(",",sentences)}] -> ");
+            return sentences.Select(item => item.ToUpper()).ToList();
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-148/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a C# Sharp program to remove all "a" in each string in given list of strings and return the new string
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_148
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Remove all ""a"" in each string in given list of strings and return the new string");
+
+            Console.WriteLine($"[{String.Join(",",RemoveChar(new List<string>(){"abc","cdaef","js","php"}))}]");
+        }
+
+        static List<string> RemoveChar(List<string> sentences)
+        {
+            Console.Write($"[{String.Join(",",sentences)}] -> ");
+            return sentences.Select(item => item.Replace('a','\0').Trim()).ToList();
+        }
+    }
+}
