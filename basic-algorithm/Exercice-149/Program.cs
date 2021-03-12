49c48458bbbc6235a5884b390f6a4681d617de56
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-12 14:49:38 +0100

    Add basic-algorithm exercice 149|150

---
+++ basic-algorithm/Exercice-149/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of integers removing those values which are less than 4
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of integers
+removing those values which are less than 4");
+
+            Console.WriteLine($"[{String.Join(",",RemoveItems(new List<int>(){0,-2,1,2,3,5,4,7,8}))}]");
+            Console.WriteLine($"[{String.Join(",",RemoveItems(new List<int>(){0,-2,1,2,3,5,4,7,8}, less:false))}]");
+            Console.WriteLine($"[{String.Join(",",RemoveItems(new List<int>(){10,-2,1,-2,3,5,-4,7,-8},comparator:2, less:false))}]");
+        }
+
+        static List<int> RemoveItems(List<int> numbers, int comparator = 4, bool less = true)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+
+            if(less)
+                return numbers.Where(item => item < comparator).ToList();
+
+            return numbers.Where(item => item >= comparator).ToList();
+        }
+    }
+}
---
+++ basic-algorithm/Exercice-150/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a C# Sharp program to create a new list from a given list of integers removing those values end with 7
+*/
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercice_150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Create a new list from a given list of integers removing those values end with 7");
+            
+            Console.WriteLine($"[{String.Join(",",RemoveIfEndWith(new List<int>(){10,22,35,47,53,67}))}]");
+            Console.WriteLine($"[{String.Join(",",RemoveIfEndWith(new List<int>(){10,22,35,47,53,67}, 2))}]");
+        }
+
+        static List<int> RemoveIfEndWith(List<int> numbers, int comparator = 7)
+        {
+            Console.Write($"[{String.Join(",",numbers)}] -> ");
+            return numbers.Where(item => item % 10 != comparator).ToList();
+        }
+    }
+}
