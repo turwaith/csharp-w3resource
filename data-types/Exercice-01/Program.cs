7cca6640502945f0c65b2fc9961064c51968c231
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-13 14:06:30 +0100

    Add data-types exercice-01

---
+++ data-types/Exercice-01/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write a C# Sharp program that takes three letters as input and display them in reverse order
+*/
+using System;
+
+namespace Exercice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Takes three letters as input and display them in reverse order");
+
+            char[] userInputs = GetUserLetter();
+            Console.WriteLine($"You entered the following letters (reversed) : [{String.Join(",",userInputs)}]");
+        }
+
+        static char[] GetUserLetter()
+        {
+            char[] userInputs = new char[3];
+
+            for(int i = 0, max = userInputs.Length; i < max; i++)
+            {
+                Console.Write("Enter a letter: ");
+                userInputs[i] = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+
+            Array.Reverse(userInputs);
+            return userInputs;
+        }
+    }
+}
