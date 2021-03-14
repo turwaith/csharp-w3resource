c209de51b37736323f1eb16d1181cf9493f69f21
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-14 14:45:42 +0100

    Add data-types exercice-03|04

---
+++ data-types/Exercice-03/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Write a C# Sharp program that takes userid and password as input (type string).
+After 3 wrong attempts, user will be rejected
+*/
+using System;
+
+namespace Exercice_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Takes userid and password as input (type string).
+After 3 wrong attempts, user will be rejected");
+
+            User userOne = new User("nicolas", "azerty");
+
+            userOne.Check();
+        }
+    }
+
+    class User
+    {
+        public string Id;
+        public string Password;
+        private int totalAttempt = 3;
+
+        public User(string id, string password)
+        {
+            Id = id;
+            Password = password;
+        }
+
+        public bool Check()
+        {
+            int count = 0;
+            string id = String.Empty,
+                   password = String.Empty;
+
+            while (count++ < totalAttempt)
+            {
+                Console.Write("Enter your user id  : ");
+                id = Console.ReadLine();
+                Console.Write("Enter your password : ");
+                password = Console.ReadLine();
+
+                if(id == Id && password == Password)
+                {
+                    Console.WriteLine($"Welcome {Id}");
+                    return true;
+                }
+
+                if(count != 0)                    
+                    Console.WriteLine($"UserId and password don't match. {totalAttempt - count} attempt left");
+            }
+
+            Console.WriteLine("All attempts used. Try later");
+            return false;
+        }
+    }
+}
---
+++ data-types/Exercice-04/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Write a C# Sharp program that takes two numbers as input and perform an operation
+    (+,-,*,x,/) on them and displays the result of that operation
+*/
+using System;
+using System.Collections.Generic;
+
+namespace Exercice_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Takes two numbers as input and perform an operation
+(+,-,*,x,/) on them and displays the result of that operation");
+
+            int a = GetOperand("Input first number: ");
+            char operation = GetOperation("Input operation: ");
+            int b = GetOperand("Input second number: ");
+
+            Console.WriteLine($"{a} {operation} {b} : {(Compute(a,b,operation))}");
+        }
+
+        static int GetOperand(string msg)
+        {
+            int operand;
+
+            do
+            {
+                Console.Write(msg);                
+            }while(!Int32.TryParse(Console.ReadLine(),out operand));
+            
+            return operand;
+        }
+
+        static char GetOperation(string msg)
+        {
+            char[] validOperation = {'+', '-', '*', '/'};
+            char operation;
+
+            Console.Write(msg);
+            operation = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if(Array.IndexOf(validOperation, operation) != -1)
+                return operation;            
+            
+            return GetOperation(msg);
+        }
+
+        static int Compute(int a, int b, char operation)
+        {
+            int result = 0;
+            switch (operation)  
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;                
+            }
+            return result;
+        }
+    }
+}
