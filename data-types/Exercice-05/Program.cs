5ecba785279b4b3c3a29613a84f4d95b805f7563
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-15 12:39:31 +0100

    Add data-types exercices 05|06

---
+++ data-types/Exercice-05/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write a C# Sharp program that takes the radius of a circle as input
+    and calculate the perimeter and area of the circle
+*/
+using System;
+
+namespace Exercice_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+
+            Console.WriteLine(@"Takes the radius of a circle as input
+    and calculate the perimeter and area of the circle");
+
+            Console.Write($"Enter the radius of the circle: ");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The area of circle with a radius of {radius}: {GetCircleArea(radius)}");
+            Console.WriteLine($"The perimeter of circle with radius of {radius}: {GetCirclePerimeter(radius)}");
+        }
+
+        static double GetCircleArea(int radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        static double GetCirclePerimeter(int radius)
+        {
+            return Math.PI * Math.Pow(radius,2);
+        }
+    }
+}
---
+++ data-types/Exercice-06/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Write a C# Sharp program to display certain values of the function
+    x = y2 + 2y + 1 (using integer numbers for y , ranging from -5 to +5)
+*/
+using System;
+
+namespace Exercice_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Display certain values of the function x = y2 + 2y + 1
+(using integer numbers for y , ranging from -5 to +5)");
+
+            Console.WriteLine("x = y² - 2y + 1");
+
+            for(int i = -5, max = 5; i <= max; i++)
+                Console.WriteLine($"[y:{i}] -> {i}*{i} - 2*{i} + 1 -> x:{((i*i) - (2*i) + 1)}");
+        }
+    }
+}
