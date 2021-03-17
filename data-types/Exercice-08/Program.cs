08acdbae68fdef40cc2ac4e510832ebe0715d5e5
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-03-17 12:28:38 +0100

    Add data-type exercice 08|09

---
+++ data-types/Exercice-08/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a C# Sharp program that takes the radius of a sphere as input and
+calculate and display the surface and volume of the sphere
+*/
+using System;
+
+namespace Exercice_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Takes the radius of a sphere as input and
+calculate and display the surface and volume of the sphere");
+
+            double radius = GetNumber("Input the radius of the sphere: ");
+
+            Console.WriteLine($"The surface of the sphere: {(4*Math.PI*Math.Pow(radius,2)), -6:##.###}");
+            Console.WriteLine($"The volume of the sphere:  {((4d/3)*Math.PI*Math.Pow(radius,3)), -6:##.###}");
+        }
+
+        static double GetNumber(string msg)
+        {
+            double userInput;
+
+            do
+            {
+                Console.Write(msg);
+            } while (!Double.TryParse(Console.ReadLine(),out userInput));
+
+            return userInput;
+        }
+    }
+}
---
+++ data-types/Exercice-09/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Write a C# Sharp program that takes a character as input and check the input
+    (lowercase) is a vowel, a digit, or any other symbol
+*/
+using System;
+
+namespace Exercice_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char userInput;
+            Console.WriteLine(@"Takes a character as input and check the input
+    (lowercase) is a vowel, a digit, or any other symbol");
+
+            Console.Write("Press a key ");
+            userInput = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if(Char.IsDigit(userInput))
+            {
+                Console.WriteLine($"The char {userInput} is a number");
+            }
+            else if("aeiou".IndexOf(userInput) != -1)
+            {
+                Console.WriteLine($"The char {userInput} is a lowercase voyel");
+            }
+            else if(Char.IsLetter(userInput))
+            {
+                Console.WriteLine($"The char {userInput} is a lowercase consonant");
+            }
+            else
+            {
+                Console.WriteLine($"The char {userInput} is a symbol");
+            }
+        }
+    }
+}
